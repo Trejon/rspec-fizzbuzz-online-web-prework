f7ff9e53ec946a6f4b927687addde56a01865ad2
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-06-03 00:50:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 
 def fizzbuzz(num)
   
- if num % 3 === 0 and num % 5 === 0
+ if num % 3 == 0 and num % 5 == 0
  "FizzBuzz"
-else if num % 5 === 0 
+else if num % 5 == 0 
  "Buzz"
-else if num % 3 === 0
+else if num % 3 == 0
 "Fizz"
 else 
   nil 
